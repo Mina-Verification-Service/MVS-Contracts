684247a566ffdc40d21387ade452be8b65d828f1
JoE11-y <Josephedoh77@gmail.com>
2024-03-22 20:28:49 +0100

    add new tests to mvs proofs

--- tests/mvsProofs.test.ts
+++ tests/mvsProofs.test.ts
@@ -1,5 +1,21 @@
-import { MVSContractV2, MVSMerkleWitnessV2, MVSProofGen } from '../build/src';
-import { Mina, PrivateKey, PublicKey, AccountUpdate, Bool, verify } from 'o1js';
+import {
+  MVSContractV2,
+  MVSMerkleWitnessV2,
+  MVSProofGen,
+  ProofRecord,
+} from '../build/src';
+import {
+  Mina,
+  PrivateKey,
+  PublicKey,
+  AccountUpdate,
+  Bool,
+  verify,
+  CircuitString,
+  Provable,
+  JsonProof,
+  Encoding,
+} from 'o1js';
 import { ZKDatabaseStorage } from 'zkdb';
 import * as fs from 'fs';
 
@@ -10,7 +26,6 @@ describe('MVS PROOF TESTS', () => {
   let controllerAccount: PublicKey,
     controllerKey: PrivateKey,
     userAccount: PublicKey,
-    userKey: PrivateKey,
     zkAppAddress: PublicKey,
     zkAppPrivateKey: PrivateKey,
     zkApp: MVSContractV2,
@@ -67,7 +82,7 @@ describe('MVS PROOF TESTS', () => {
   });
 
   it('should fail if commitment root is tried to be set by non controller', async () => {
-    ({ privateKey: userKey, publicKey: userAccount } = Local.testAccounts[1]);
+    ({ publicKey: userAccount } = Local.testAccounts[1]);
     // get storage root from zkdb instance
     const storageRoot = await zkdb.getMerkleRoot();
     await expect(
@@ -102,6 +117,73 @@ describe('MVS PROOF TESTS', () => {
       Bool(true)
     );
     const ok = await verify(userProof.toJSON(), verificationKey);
+    console.log(userProof.toJSON());
+    expect(ok).toBeTruthy();
+  });
+
+  it('should not generate valid proof if ml result is false', async () => {
+    // get curr of users
+    const userCount = zkApp.numOfUsers.get();
+    // get witness for user index
+    const witness = new MVSMerkleWitnessV2(
+      await zkdb.getWitnessByIndex(userCount.toBigInt())
+    );
+    let commitment = zkApp.storageRoot.get();
+    await MVSProofGen.compile();
+    await expect(
+      MVSProofGen.getProof(witness, commitment, Bool(false))
+    ).rejects.toThrow();
+  });
+
+  it('should store proof properly in zkdb', async () => {
+    // get curr of users
+    const userCount = zkApp.numOfUsers.get();
+    // get witness for user index
+    const witness = new MVSMerkleWitnessV2(
+      await zkdb.getWitnessByIndex(userCount.toBigInt())
+    );
+    let commitment = zkApp.storageRoot.get();
+    const { verificationKey } = await MVSProofGen.compile();
+    const userProof = await MVSProofGen.getProof(
+      witness,
+      commitment,
+      Bool(true)
+    );
+
+    const proofJson = userProof.toJSON();
+    const proofString = JSON.stringify(proofJson);
+
+    const encoder = new TextEncoder();
+    const bytes = encoder.encode(proofString);
+
+    const encodedProof = Encoding.Bijective.Fp.fromBytes(bytes);
+    const ok = await verify(proofJson, verificationKey);
+
+    const proofRecord = new ProofRecord({
+      userId: CircuitString.fromString('dummy'),
+      proof: encodedProof,
+    });
+
+    zkdb.add(proofRecord);
+    expect(ok).toBeTruthy();
+  });
+
+  it('should get proof properly in zkdb', async () => {
+    const findRecord = zkdb.findOne('userId', 'dummy');
+
+    if (findRecord.isEmpty()) {
+      throw new Error('User does not exist on DB');
+    }
+    // load instance
+    const proofRecord = await findRecord.load(ProofRecord);
+    const { verificationKey } = await MVSProofGen.compile();
+    const proofRecordJson = proofRecord.json();
+    const decodedProof = Encoding.Bijective.Fp.toBytes(proofRecordJson.proof);
+    // // Decode the bytes back to a string
+    const decoder = new TextDecoder();
+    const decodedStr = decoder.decode(decodedProof);
+    const proofJson = JSON.parse(decodedStr) as JsonProof;
+    const ok = await verify(proofJson, verificationKey);
     expect(ok).toBeTruthy();
   });
 });
